4ae643466c21f4d58a5b4a2b487aa1ed30e18a6d
victorlrdz26 <44705353+victorlrdz26@users.noreply.github.com>
2018-11-03 18:42:37 -0600

    Add files via upload

---
+++ web/index.php
@@ -0,0 +1,23 @@
+<?php
+
+$title = "Home";
+$content = '<img src="Imagenes/coffee1.png" class="imgLeft" />
+<h3> Cafe1 </h3>
+<p>
+    agregar descp
+</p>
+
+<img src="Imagenes/coffee2.png" class="imgRight" />
+<h3> Cafe2 </h3>
+<p>
+    agregar descp
+</p>
+
+<img src="Imagenes/coffee3.png" class="imgLeft" />
+<h3> Cafe3 </h3>
+<p>
+    agregar descp
+</p>';
+
+include 'Template.php';
+?>
